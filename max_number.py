5506cc305c8a5c2e68aeba8cbaa00006c5b7b154
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2021-01-12 19:53:02 +0000

    added max_number.py

---
+++ max_number.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Created by Marlon Poddalgoda
+# Created on January 2021
+# This program prints 10 random integers and finds the average
+
+import random
+
+
+def max_value(random_list):
+    # This function finds the largest number
+
+    max_num = random_list[0]
+
+    for loop_counter in range(0, len(random_list)):
+        if max_num >= random_list[loop_counter]:
+            continue
+        elif max_num < random_list[loop_counter]:
+            max_num = random_list[loop_counter]
+
+    return max_num
+
+
+def main():
+    # This function prints 10 random integers and output
+
+    print("This program prints 10 random integers and"
+          " finds the largest number.")
+    print("")
+
+    random_list = []
+
+    print("Here are 10 random numbers:")
+
+    # process
+    for loop_counter in range(0, 10):
+        # random number
+        random_number = random.randint(1, 100)
+        random_list.append(random_number)
+        print(random_number)
+
+    # call function
+    largest_num = max_value(random_list)
+
+    print("")
+    print("The largest number is: {0}".format(largest_num))
+
+
+if __name__ == "__main__":
+    main()
